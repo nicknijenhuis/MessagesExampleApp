f370f31023de367a8d1eb041429f64869b2daeb3
Nick Nijenhuis <nick.nijenhuis@qnh.nl>
2017-06-06 22:47:25 +0200

    Messages API

---
+++ Messages/Controllers/MessagesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MessagesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        private static readonly List<Message> Messages = new List<Message>
+        {
+            new Message {Id = 1, Date = DateTime.UtcNow, Title = "Message 1", Description = "This is the first message"}
+        };
+
+
+        // GET api/messages
+        [HttpGet]
+        public IEnumerable<Message> Get()
+        {
+            return Messages.OrderByDescending(x => x.Date);
+        }
+
+        // GET api/messages/5
+        [HttpGet("{id}")]
+        public Message Get(int id) => Messages.SingleOrDefault(x => x.Id == id);
+
+        // POST api/messages
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/messages/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/messages/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
